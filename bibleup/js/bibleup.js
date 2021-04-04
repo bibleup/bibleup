6e883ff621aa38e905764a9d85e6421c65609215
Bukunmikuti <bukunmikuti@gmail.com>
2021-04-04 14:45:04 +0100

    devDependencies: snowpack 3.2.2

--- bibleup/js/bibleup.js
+++ bibleup/js/bibleup.js
@@ -266,7 +266,7 @@ positionPopup(e) {
 	} else {
 		let offsetBy = popWidth - remainingSpace;
 		let adjust = rectLeft - offsetBy
-		popup.style.left = `${adjust + 'px'}`;
+		popup.style.left = `${adjust - 100 + 'px'}`;
 	}
 	
 	
@@ -365,7 +365,6 @@ closePopup(e) {
 	
 	
 
-	
 	
 	
 	
