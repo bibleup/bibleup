a4c87af9ae2ea2bd92d68b3a2ff6923a4cc262e0
Bukunmikuti <bukunmikuti@gmail.com>
2021-04-26 12:57:52 +0100

    Very minor changes in demo

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ let test = document.querySelector('#test');
 let testPanel = document.querySelector('#test-panel');
 let body = document.querySelector('body')
 
-let bibleup = new BibleUp(testPanel, {linkStyle: 'classic', popup: 'inline', darkTheme: 'u', bu_allow:['H2']});
-bibleup.create();
+console.log(typeof(window.BibleUP))
 
 
 
