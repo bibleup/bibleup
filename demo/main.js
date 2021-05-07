97d431bcd3f673f41665e9ce7a7371a8490078b5
Bukunmikuti <bukunmikuti@gmail.com>
2021-05-07 03:21:53 +0100

    Updated readme, BibleUp can now reference abbreviations with a fullstop

--- demo/main.js
+++ demo/main.js
@@ -1,13 +1,16 @@
 import BibleUp from '../dist/bibleup.esm.min.js';
+//import BibleUp from '../bibleup/js/bibleup.js'
 //if (eruda) eruda.init();
 
 let test = document.querySelector('#test');
 let testPanel = document.querySelector('#test-panel');
 let body = document.querySelector('body')
 
-let bibleup = new BibleUp(body)
+let bibleup = new BibleUp(body, {
+	linkStyle: 'myStyle'
+})
 bibleup.create()
-//console.log(BibleUp)
+console.log(bibleup.getOptions)
 
 
 
