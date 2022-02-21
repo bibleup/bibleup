0f88d0a81a7501194f40f440edcc7a7de089796a
Bukunmikuti <Bukunmikuti@gmail.com>
2022-02-21 17:37:00 +0100

    Update sync

--- demo/main.js
+++ demo/main.js
@@ -1,8 +1,6 @@
-import BibleUp from '../dist/esm/bibleup.es.min.js';
-//import BibleUp from '../bibleup/js/bibleup.js'
+//import BibleUp from '../dist/esm/bibleup.es.min.js';
+import BibleUp from '../bibleup/js/bibleup.js'
 
-
-if (eruda) eruda.init();
 let test = document.querySelector('#test');
 let testPanel = document.querySelector('#test-panel');
 let body = document.querySelector('body')
