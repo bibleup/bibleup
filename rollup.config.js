4d314d6a4fa38a4d0289457597c5310f0a46bb95
Bukunmikuti <bukunmikuti@gmail.com>
2022-05-09 21:39:09 +0100

    added rollup-babel to compile build | restructured build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,23 @@
 import { terser } from "rollup-plugin-terser";
 import less from "rollup-plugin-less";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+
+const babelConfig = babel({
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          ie: "11",
+        },
+      },
+    ],
+  ],
+  babelHelpers: "bundled",
+  exclude: "node_modules/**",
+})
+
 
 export default [
   {
@@ -9,15 +27,33 @@ export default [
         file: "./dist/esm/bibleup.esm.js",
         format: "es",
         name: "BibleUp",
-      },
+      }
+    ],
+    plugins: [
+      // no minified, no css
+      resolve(),
+      babelConfig
+    ],
+  },
+
+  {
+    input: "./bibleup/js/bibleup.js",
+    output: [
       {
-        file: "./dist/umd/bibleup-core.js",
+        file: "./dist/umd/bibleup-core.min.js",
         format: "umd",
         name: "BibleUp", //name of the global object
         sourcemap: true,
       },
-    ]
+    ],
+    plugins: [
+      // minified but no css
+      resolve(),
+      babelConfig,
+      terser(),
+    ],
   },
+
   {
     input: "./bibleup/js/main.js",
     output: [
@@ -29,11 +65,13 @@ export default [
       },
     ],
     plugins: [
+      // minified and css
       less({
         insert: true,
-        output: './dist/css/bibleup.css',
+        output: "./dist/css/bibleup.css",
       }),
+      babelConfig,
       terser(),
     ],
-  }
+  },
 ];
