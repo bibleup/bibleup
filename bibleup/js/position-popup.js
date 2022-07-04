5ad45c58a926e758bfeed6df5e0c206311674ca1
Bukunmikuti <bukunmikuti@gmail.com>
2022-07-04 19:21:25 +0100

    fixed eslint

--- bibleup/js/position-popup.js
+++ bibleup/js/position-popup.js
@@ -5,58 +5,58 @@
  * use classic() to set position on custom syled popups
  */
 
-let positionPopup = (e, popup) => {
-  if (popup == 'classic') {
-    classic(e);
+const positionPopup = (e, popup) => {
+  if (popup === 'classic') {
+    classic(e)
   }
-  if (popup == 'inline') {
-    inline(e);
+  if (popup === 'inline') {
+    inline(e)
   }
-  return false;
-};
+  return false
+}
 
 /**
  * Adjust popup to left or right of click
  */
-let adjustPopupToLeft = (pos) => {
-  let [width, rectLeft, popup, popWidth] = pos;
-  let remainingSpace = width - rectLeft;
+const adjustPopupToLeft = (pos) => {
+  const [width, rectLeft, popup, popWidth] = pos
+  const remainingSpace = width - rectLeft
 
   if (remainingSpace > popWidth) {
     if (rectLeft < 0) {
-		popup.style.left = `${0 + 'px'}`;
+      popup.style.left = `${0 + 'px'}`
     } else {
-		popup.style.left = `${rectLeft + 'px'}`;
-	}
+      popup.style.left = `${rectLeft + 'px'}`
+    }
   } else {
-    let offsetBy = popWidth - remainingSpace;
-    let adjust = rectLeft - offsetBy;
-    popup.style.left = `${adjust + 'px'}`;
+    const offsetBy = popWidth - remainingSpace
+    const adjust = rectLeft - offsetBy
+    popup.style.left = `${adjust + 'px'}`
   }
-};
+}
 
 /**
  * adjust popup to bottom or top of link according to space remaining
  * Popup positions to top of link by default and fallbacks to bottom if there is no space.
  */
-let adjustPopupToBottom = (pos) => {
-  let [height, rectBottom, popup, popHeight, realTop, rectHeight] = pos;
-  let bottomSpace = height - rectBottom;
-  let topSpace = height - bottomSpace;
+const adjustPopupToBottom = (pos) => {
+  const [height, rectBottom, popup, popHeight, realTop, rectHeight] = pos
+  const bottomSpace = height - rectBottom
+  const topSpace = height - bottomSpace
 
   if (topSpace > popHeight + 50) {
-    let adjust = realTop - popHeight;
-    popup.style.top = `${adjust - 5 + 'px'}`;
+    const adjust = realTop - popHeight
+    popup.style.top = `${adjust - 5 + 'px'}`
   } else {
-    popup.style.top = `${realTop + (rectHeight + 5) + 'px'}`;
+    popup.style.top = `${realTop + (rectHeight + 5) + 'px'}`
   }
-};
+}
 
-let getPosition = (e) => {
-  //get window dimensions
-  let rect = e.target.getBoundingClientRect();
-  let popup = document.getElementById('bu-popup');
-  let rectTop = rect.top;
+const getPosition = (e) => {
+  // get window dimensions
+  const rect = e.target.getBoundingClientRect()
+  const popup = document.getElementById('bu-popup')
+  const rectTop = rect.top
 
   return {
     popup,
@@ -68,21 +68,21 @@ let getPosition = (e) => {
     popHeight: popup.offsetHeight,
     rectBottom: rect.bottom,
     rectLeft: Math.round(rect.left),
-    realTop: window.scrollY + rectTop,
-  };
-};
+    realTop: window.scrollY + rectTop
+  }
+}
 
-let classic = (e) => {
-  let el = getPosition(e);
-  adjustPopupToLeft([el.width, el.rectLeft, el.popup, el.popWidth]);
-  adjustPopupToBottom([el.height, el.rectBottom, el.popup, el.popHeight, el.realTop, el.rect.height]);
-};
+const classic = (e) => {
+  const el = getPosition(e)
+  adjustPopupToLeft([el.width, el.rectLeft, el.popup, el.popWidth])
+  adjustPopupToBottom([el.height, el.rectBottom, el.popup, el.popHeight, el.realTop, el.rect.height])
+}
 
-let inline = (e) => {
-  let el = getPosition(e);
-  el.popWidth += 10;
-  adjustPopupToLeft([el.width, el.rectLeft, el.popup, el.popWidth]);
-  adjustPopupToBottom([el.height, el.rectBottom, el.popup, el.popHeight, el.realTop, el.rect.height]);
-};
+const inline = (e) => {
+  const el = getPosition(e)
+  el.popWidth += 10
+  adjustPopupToLeft([el.width, el.rectLeft, el.popup, el.popWidth])
+  adjustPopupToBottom([el.height, el.rectBottom, el.popup, el.popHeight, el.realTop, el.rect.height])
+}
 
-export default positionPopup;
+export default positionPopup
