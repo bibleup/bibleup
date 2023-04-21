bbc7296b5205462feef5faa2359c3252a86f121c
Bukunmikuti <bukunmikuti@gmail.com>
2023-04-21 08:21:41 +0100

    migrate rollup config to typescript

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,10 +1,10 @@
-import terser from '@rollup/plugin-terser'
-import less from 'rollup-plugin-less'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import babel from '@rollup/plugin-babel'
-import license from 'rollup-plugin-license'
-import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser';
+import less from 'rollup-plugin-less';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import license from 'rollup-plugin-license';
+import typescript from '@rollup/plugin-typescript';
 
 const babelConfig = babel({
   presets: [
@@ -13,14 +13,14 @@ const babelConfig = babel({
       {
         targets: '>= 0.5%, not dead',
         useBuiltIns: 'usage',
-        corejs: '3.30.0'
-      }
-    ]
+        corejs: '3.30.0',
+      },
+    ],
   ],
   plugins: ['@babel/plugin-transform-runtime'],
   exclude: '/node_modules/**',
-  babelHelpers: 'runtime'
-})
+  babelHelpers: 'runtime',
+});
 
 const addLicense = license({
   banner: `
@@ -28,8 +28,8 @@ const addLicense = license({
   Copyright 2023 BibleUp and contributors
   Repository URL: https://github.com/Bibleup/bibleup.ts.git
   Date: <%= moment().format('DD-MM-YYYY') %>
-  `
-})
+  `,
+});
 
 export default [
   {
@@ -39,12 +39,10 @@ export default [
         file: './dist/esm/bibleup.esm.js',
         format: 'es',
         name: 'BibleUp',
-        sourcemap: true
-      }
+        sourcemap: true,
+      },
     ],
-    plugins: [
-      typescript()
-    ]
+    plugins: [typescript()],
   },
 
   {
@@ -54,20 +52,20 @@ export default [
         file: './dist/umd/bibleup-core.min.js',
         format: 'umd',
         name: 'BibleUp', // name of the global object
-        sourcemap: true
-      }
+        sourcemap: true,
+      },
     ],
     plugins: [
       // minified but no css
       typescript(),
       nodeResolve(),
       commonjs({
-        include: 'node_modules/**'
+        include: 'node_modules/**',
       }),
       babelConfig,
       terser(),
-      addLicense
-    ]
+      addLicense,
+    ],
   },
 
   {
@@ -77,23 +75,23 @@ export default [
         file: './dist/umd/bibleup.min.js',
         format: 'umd',
         name: 'BibleUp', // name of the global object
-        sourcemap: true
-      }
+        sourcemap: true,
+      },
     ],
     plugins: [
       // minified and css
       typescript(),
       less({
         insert: true,
-        output: './dist/css/bibleup.css'
+        output: './dist/css/bibleup.css',
       }),
       nodeResolve(),
       commonjs({
-        include: 'node_modules/**'
+        include: 'node_modules/**',
       }),
       babelConfig,
       terser(),
-      addLicense
-    ]
-  }
-]
+      addLicense,
+    ],
+  },
+];
