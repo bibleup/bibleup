13191e3bd210636f9d0c70e380c36f1cd33dde95
Bukunmikuti <bukunmikuti@gmail.com>
2023-10-02 19:03:14 +0100

    add config sample

---
+++ bibleup/helper/config-sample.ts
@@ -0,0 +1,16 @@
+/**
+ * Bible API Configuration
+ *
+ * To use the Bible.api service, you need to obtain an API key.
+ * Follow these steps to get your API key:
+ *
+ * 1. Visit https://scripture.api.bible/ and sign up for an account.
+ * 2. Once you have an account, log in and go to your profile settings.
+ * 3. Generate a new API key and copy it.
+ * 4. Replace 'YOUR_BIBLE_API_KEY' below with your actual API key.
+ *
+ * Note: Keep your API key secure and do not share it publicly.
+ */
+
+const apiKey = 'YOUR_API_KEY'
+export default apiKey
